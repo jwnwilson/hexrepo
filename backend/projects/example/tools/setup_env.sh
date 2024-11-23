6e4d47917cefdccc2373c66e37c9ef81af7d2893
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-11-23 14:06:13 +0000

    Install dev dependencies

--- backend/projects/example/tools/setup_env.sh
+++ backend/projects/example/tools/setup_env.sh
@@ -14,4 +14,4 @@ source ${ROOT_DIR}/${VENV}/bin/activate && \
 export SYSTEM_VERSION_COMPAT=1 && \
 pip install poetry && \
 poetry lock && \
-poetry install --all-extras
+poetry install --with dev --sync
