85a8ca65e7e4fa5221c20bf0c336756e97612228
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-11-24 23:28:26 +0000

    Fixing bash condition

--- backend/libs/src/adaptor/db/tools/setup_env.sh
+++ backend/libs/src/adaptor/db/tools/setup_env.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
 if [[ -z "${GITHUB_ACTIONS}" ]]; then
+    echo "Running locally, creating venv."
+else
     echo "Running on github skipping venv creation."
     exit 0
 fi
