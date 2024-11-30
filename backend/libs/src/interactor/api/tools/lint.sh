2fd600a866eeae2e401a18651859ee1bebc78837
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-11-30 15:07:15 +0000

    Adding link check command

--- backend/libs/src/interactor/api/tools/lint.sh
+++ backend/libs/src/interactor/api/tools/lint.sh
@@ -11,7 +11,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 
-APP_FOLDER="monorepo_stoage tests"
+APP_FOLDER="monorepo_api tests"
 
 if [[ -z "${GITHUB_ACTIONS}" ]]; then
     echo "Running locally, activating venv."
