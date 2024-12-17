1d24e6409002159e6444afe0863ee1ca8d9ba269
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-17 09:12:41 +0000

    Fixing deploy script bugs

--- tools/bash_scripts/deploy.sh
+++ tools/bash_scripts/deploy.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-region="eu-west-1"
-aws_ecr="675468650888.dkr.ecr.eu-west-1.amazonaws.com"
-ecr_repo_name="monorepo-example"
-latest_image=`docker images -q example`
-# Use last commit datetime as git tag
-docker_tag=$(git log -n1 --pretty='format:%cd' --date=format:'%Y%m%d%H%M%S')
-
 # Docker login
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 675468650888.dkr.ecr.eu-west-1.amazonaws.com
 
 docker compose build
+
+region="eu-west-1"
+aws_ecr="675468650888.dkr.ecr.eu-west-1.amazonaws.com"
+ecr_repo_name="monorepo-example"
+# Use last commit datetime as git tag
+docker_tag=$(git log -n1 --pretty='format:%cd' --date=format:'%Y%m%d%H%M%S')
+latest_image=`docker images -q example`
 
 # tag and push docker image
 docker tag "${latest_image}" "${aws_ecr}/${ecr_repo_name}:latest"
@@ -20,7 +20,7 @@ docker tag "${latest_image}" "${aws_ecr}/${ecr_repo_name}:${docker_tag}"
 docker push "${aws_ecr}/${ecr_repo_name}:latest"
 docker push "${aws_ecr}/${ecr_repo_name}:${docker_tag}"
 
-if [[ ${NO_INPUT} == "true" ]] then;
+if [ "${NO_INPUT}" == "true" ]; then
     make tf_apply_no_input TF_VAR_docker_tag=${docker_tag}
 else
     make tf_apply TF_VAR_docker_tag=${docker_tag}
