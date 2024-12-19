59270835c7874cae9d94095c2058e63f903c9331
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-19 18:40:27 +0000

    Formatting

--- libs/src/adaptor/db/monorepo_db/__init__.py
+++ libs/src/adaptor/db/monorepo_db/__init__.py
@@ -1,1 +1,3 @@
-from .interface import UOW as UOW, Repository as Repository, PaginatedData as PaginatedData
+from .interface import UOW as UOW
+from .interface import PaginatedData as PaginatedData
+from .interface import Repository as Repository
