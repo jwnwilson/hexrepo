9eecce0b69bcea87542e44a788b14f20d380ca45
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-24 11:18:20 +0000

    Adding web crawler project

---
+++ projects/web_crawler/infra/tf/aws/environment/output.tf
@@ -0,0 +1,5 @@
+
+# output "db_migrator_lambda_name" {
+#   value = module.migrate_db_task.lambda_function_name
+# }
+
---
+++ projects/web_crawler/src/app/interactor/api/fastapi/dependencies.py
@@ -0,0 +1,32 @@
+import logging
+import uuid
+from collections.abc import Generator
+from pydantic import BaseModel
+from monorepo_db import UOW, Repository
+from monorepo_db.sql.stub import StubbedRepository
+
+from app.domain.example import ExampleDTO
+
+logger = logging.getLogger(__name__)
+
+
+class StubbedExampleRepository(StubbedRepository):
+    model_dto: ExampleDTO = ExampleDTO
+
+    def _generate_fake_dto(self, obj_in: BaseModel = None) -> BaseModel:
+        return self.model_dto(
+            id=uuid.uuid4(),
+            name="test",
+            url="https://test.com",
+            location="test location",
+        )
+
+
+class StubbedUOW(UOW):
+    @property
+    def example(self) -> Repository:
+        return StubbedExampleRepository()
+
+
+def get_uow() -> Generator[UOW, None, None]:
+    yield StubbedUOW(db_url="test")
