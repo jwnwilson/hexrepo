52165611effe64c42ee90901b17a0b2552903b49
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-25 00:31:47 +0000

    Simplifying venv command

--- tools/bash_scripts/setup_env.sh
+++ tools/bash_scripts/setup_env.sh
@@ -1,15 +1,3 @@
 #! /bin/bash
-VENV=.venv
-CURRENT_DIR=.
 
-if [ -d "./.venv" ]; then
-    # Venv already exists, skip creation
-    # echo "Virtual environment already exists, skipping venv creation."
-    exit 0
-fi
-
-echo "Activating virtual environment in directory: ${PWD}"
-
-python3.12 -m venv ${VENV}; \
-source ${CURRENT_DIR}/${VENV}/bin/activate && \
 uv sync --all-extras
