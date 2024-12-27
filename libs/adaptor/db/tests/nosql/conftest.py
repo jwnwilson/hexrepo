1d7307d6205031ceac011ba83d16285086943e1c
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-27 20:01:28 +0000

    Updating docker compose to see if tests can connect

--- libs/adaptor/db/tests/nosql/conftest.py
+++ libs/adaptor/db/tests/nosql/conftest.py
@@ -7,26 +7,26 @@ from monorepo_db.nosql import DynamoUOW, MongoUOW
 from monorepo_db.sql.models.example import ExampleCreateDTO, ExampleDTO
 
 
-# @pytest.fixture
-# def uow_dynamo() -> Generator[UOW, None, None]:
-#     db_url = "http://localhost:8000"
-#     uow = DynamoUOW(db_url=db_url)
-#     # with uow.transaction():
-#     yield uow
+@pytest.fixture
+def uow_dynamo() -> Generator[UOW, None, None]:
+    db_url = "http://0.0.0.0:8000"
+    uow = DynamoUOW(db_url=db_url)
+    # with uow.transaction():
+    yield uow
 
 
-# @pytest.fixture
-# def uow_mongo() -> Generator[UOW, None, None]:
-#     db_url = "mongodb://localhost:27017/test_db"
-#     uow = MongoUOW(db_url=db_url)
-#     # with uow.transaction():
-#     yield uow
+@pytest.fixture
+def uow_mongo() -> Generator[UOW, None, None]:
+    db_url = "mongodb://0.0.0.0:27017/test_db"
+    uow = MongoUOW(db_url=db_url)
+    # with uow.transaction():
+    yield uow
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def create_tables_dynamo(uow_dynamo: UOW) -> None:
-#     uow_dynamo.drop_all()
-#     uow_dynamo.create_all()
+@pytest.fixture(scope="function", autouse=True)
+def create_tables_dynamo(uow_dynamo: UOW) -> None:
+    uow_dynamo.drop_all()
+    uow_dynamo.create_all()
 
 
 @pytest.fixture(scope="function", autouse=True)
