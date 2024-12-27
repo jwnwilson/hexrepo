b5098a4450e7e244072f98a1c3996d1bd2088a3d
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-12-27 20:36:23 +0000

    debugging ci

--- libs/adaptor/db/tests/nosql/conftest.py
+++ libs/adaptor/db/tests/nosql/conftest.py
@@ -1,58 +1,58 @@
-from typing import Dict, Generator
+# from typing import Dict, Generator
 
-import pytest
+# import pytest
 
-from monorepo_db import UOW
-from monorepo_db.nosql import DynamoUOW, MongoUOW
-from monorepo_db.sql.models.example import ExampleCreateDTO, ExampleDTO
-
-
-@pytest.fixture
-def uow_dynamo() -> Generator[UOW, None, None]:
-    db_url = "http://localhost:8000"
-    uow = DynamoUOW(db_url=db_url)
-    # with uow.transaction():
-    yield uow
+# from monorepo_db import UOW
+# from monorepo_db.nosql import DynamoUOW, MongoUOW
+# from monorepo_db.sql.models.example import ExampleCreateDTO, ExampleDTO
 
 
-@pytest.fixture
-def uow_mongo() -> Generator[UOW, None, None]:
-    db_url = "mongodb://localhost:27017/test_db"
-    uow = MongoUOW(db_url=db_url)
-    # with uow.transaction():
-    yield uow
+# @pytest.fixture
+# def uow_dynamo() -> Generator[UOW, None, None]:
+#     db_url = "http://localhost:8000"
+#     uow = DynamoUOW(db_url=db_url)
+#     # with uow.transaction():
+#     yield uow
 
 
-@pytest.fixture(scope="function", autouse=True)
-def create_tables_dynamo(uow_dynamo: UOW) -> None:
-    uow_dynamo.drop_all()
-    uow_dynamo.create_all()
+# @pytest.fixture
+# def uow_mongo() -> Generator[UOW, None, None]:
+#     db_url = "mongodb://localhost:27017/test_db"
+#     uow = MongoUOW(db_url=db_url)
+#     # with uow.transaction():
+#     yield uow
 
 
-@pytest.fixture(scope="function", autouse=True)
-def create_tables_mongo(uow_mongo: UOW) -> None:
-    uow_mongo.drop_all()
-    uow_mongo.create_all()
+# @pytest.fixture(scope="function", autouse=True)
+# def create_tables_dynamo(uow_dynamo: UOW) -> None:
+#     uow_dynamo.drop_all()
+#     uow_dynamo.create_all()
 
 
-@pytest.fixture
-def example_records_dynamo(uow_dynamo: UOW) -> Dict[str, ExampleDTO]:
-    example_1: ExampleDTO = uow_dynamo.example.create(
-        ExampleCreateDTO(name="example1", url="example1.com")
-    )
-    example_2: ExampleDTO = uow_dynamo.example.create(
-        ExampleCreateDTO(name="example2", url="example2.com")
-    )
-
-    return {"example_1": example_1, "example_2": example_2}
+# @pytest.fixture(scope="function", autouse=True)
+# def create_tables_mongo(uow_mongo: UOW) -> None:
+#     uow_mongo.drop_all()
+#     uow_mongo.create_all()
 
 
-@pytest.fixture
-def example_records_mongo(uow_mongo: UOW) -> Dict[str, ExampleDTO]:
-    example_1: ExampleDTO = uow_mongo.example.create(
-        ExampleCreateDTO(name="example1", url="example1.com")
-    )
-    example_2: ExampleDTO = uow_mongo.example.create(
-        ExampleCreateDTO(name="example2", url="example2.com")
-    )
-    return {"example_1": example_1, "example_2": example_2}
+# @pytest.fixture
+# def example_records_dynamo(uow_dynamo: UOW) -> Dict[str, ExampleDTO]:
+#     example_1: ExampleDTO = uow_dynamo.example.create(
+#         ExampleCreateDTO(name="example1", url="example1.com")
+#     )
+#     example_2: ExampleDTO = uow_dynamo.example.create(
+#         ExampleCreateDTO(name="example2", url="example2.com")
+#     )
+
+#     return {"example_1": example_1, "example_2": example_2}
+
+
+# @pytest.fixture
+# def example_records_mongo(uow_mongo: UOW) -> Dict[str, ExampleDTO]:
+#     example_1: ExampleDTO = uow_mongo.example.create(
+#         ExampleCreateDTO(name="example1", url="example1.com")
+#     )
+#     example_2: ExampleDTO = uow_mongo.example.create(
+#         ExampleCreateDTO(name="example2", url="example2.com")
+#     )
+#     return {"example_1": example_1, "example_2": example_2}
