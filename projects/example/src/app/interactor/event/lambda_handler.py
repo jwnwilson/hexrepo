9637bdb795057d2cbab28470d1d949bc0dc8e299
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-01-01 19:43:56 +0000

    Adding basic lambda handler

--- projects/example/src/app/interactor/event/lambda_handler.py
+++ projects/example/src/app/interactor/event/lambda_handler.py
@@ -1,4 +1,4 @@
+from .tasks.app import app
 
 def handler(event, context):
-    
-    return lambda_handler(event, context)
+    return app.handle(event)
--- projects/example/src/app/interactor/event/tasks/app.py
+++ projects/example/src/app/interactor/event/tasks/app.py
@@ -1,7 +1,11 @@
 from hexrepo_task.app import TaskApp, TaskDTO
 from .dependencies import get_queue_uow, get_task_queue
 
-app = TaskApp(get_uow=get_queue_uow, get_queue=get_task_queue)
+
+app = TaskApp(
+    get_uow=get_queue_uow,
+    get_queue=get_task_queue
+)
 
 
 @app.task
