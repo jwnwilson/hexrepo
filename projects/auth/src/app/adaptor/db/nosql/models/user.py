b6eab643e60382dcc9d43c713358cd9a592e3842
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-01-28 08:15:54 +0000

    Adding id created at and updated at

--- projects/auth/src/app/adaptor/db/nosql/models/user.py
+++ projects/auth/src/app/adaptor/db/nosql/models/user.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from .base_model import Base
 
 
-class UserPermissionDTO(BaseModel):
+class UserPermissionDTO(Base):
     username: str
     name: str
     email: str
