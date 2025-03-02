d17a14943ad32aeb18b72a63c3dbd3d80eb0e7e0
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-03-02 22:48:26 +0000

    Adding basic cache that works for lambda to secret lib

--- libs/adaptor/cloud/hexrepo_cloud/secrets/aws.py
+++ libs/adaptor/cloud/hexrepo_cloud/secrets/aws.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import os
 from typing import Dict, Optional
@@ -9,25 +10,29 @@ from .interface import SecretAdaptor
 logger = logging.getLogger(__name__)
 
 
-def aws_cache(secret_name: str) -> Optional[str]:
+def secret_cache(func):
     # Check if the secret is already cached in /tmp this is the cheapest and quickest method
     # for caching in aws lambdas
-    if secret_name in set(os.listdir("/tmp")):
-        with open(f"/tmp/{secret_name}", "r") as f:
-            return f.read()
-    else:
-        return None  
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Optional[str]:
+        secret_name: str = args[1]
+        if secret_name in set(os.listdir("/tmp")):
+            logger.info(f"Secret: {secret_name} retrieved from file cache.")
+            with open(f"/tmp/{secret_name}", "r") as f:
+                return f.read()
+        secret_value = func(*args, **kwargs)
+        if secret_value:
+            with open(f"/tmp/{secret_name}", "w") as f:
+                f.write(secret_value)
+    return wrapper
 
 
 class AWSSecretAdaptor(SecretAdaptor):
     def __init__(self) -> None:
         self.client = boto3.client("secretsmanager")
 
+    @secret_cache
     def get_secret(self, secret_name: str) -> str:
-        secret_value: str = aws_cache(secret_name)
-        if secret_value:
-            logger.info(f"Secret: {secret_name} retrieved from file cache.")
-            return secret_value
         logger.info(f"Getting secret: {secret_name}")
         try:
             get_secret_value_response: Dict[str, str] = self.client.get_secret_value(
