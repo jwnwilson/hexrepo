1f3e264b728d16638677883b7bdfe61328fb9780
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-03-02 21:13:34 +0000

    Adding str to args and kwargs before hashing

--- libs/adaptor/cloud/hexrepo_cloud/secrets/aws.py
+++ libs/adaptor/cloud/hexrepo_cloud/secrets/aws.py
@@ -16,7 +16,7 @@ def aws_secret_cache(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         # hash the function name and the arguments to create a unique key
-        key = f"{func.__name__}-{hash(args)}- {hash(kwargs)}"
+        key = f"{func.__name__}-{hash(str(args))}- {hash(str(kwargs))}"
         if key in set(os.listdir("/tmp")):
             logger.info(f"Cache hit for {key}")
             with open(f"/tmp/{key}", "r") as f:
