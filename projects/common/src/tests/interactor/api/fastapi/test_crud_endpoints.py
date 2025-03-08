6d41ab04db12854a2c5b6f05af2fdedf09258d52
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-03-08 12:14:57 +0000

    WIP adding logic to test read logic

--- projects/common/src/tests/interactor/api/fastapi/test_crud_endpoints.py
+++ projects/common/src/tests/interactor/api/fastapi/test_crud_endpoints.py
@@ -26,3 +26,13 @@ def test_crud_create(client: TestClient, endpoint: Dict):
     reponse_data = response.json()
     for key, value in create_payload.items():
         assert reponse_data[key] == value
+
+
+@pytest.mark.parametrize("endpoint", API_ENDPOINT_TEST_DATA.keys())
+def test_crud_read_many(client: TestClient, test_data_generator, endpoint: Dict):
+    router: CrudRouter = API_ENDPOINT_TEST_DATA[endpoint]
+    record = test_data_generator(payload=router.create_schema)
+    response = client.get(f"/api/v1/{endpoint}/")
+    assert response.status_code == 200
+    reponse_data = response.json()
+    assert reponse_data["results"] == record.model_dump()
