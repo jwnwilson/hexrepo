40fe8a19c6fc27b0777595e0f6269ce0fdc1128a
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-04-06 21:11:52 +0100

    fixing typo

--- projects/common/src/app/config.py
+++ projects/common/src/app/config.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 # Silence noisy logs from faker
 logging.getLogger("faker.factory").setLevel(logging.ERROR)
 
-ENV = os.environ("ENVIRONMENT", "local")
+ENV = os.environ.get("ENVIRONMENT", "local")
 load_dotenv(os.environ.get("ENV_FILE", f"./env/{ENV}.env"))
 
 
