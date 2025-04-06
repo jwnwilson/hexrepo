a181d2d8b0cca9ce1ea697e19e4972d1f8329639
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-04-06 14:20:06 +0100

    Adding dummy scheduled event handler and adding correctation id to request-id header

--- libs/interactor/api/hexrepo_api/lambda_wrapper.py
+++ libs/interactor/api/hexrepo_api/lambda_wrapper.py
@@ -1,9 +1,37 @@
 import logging
+from typing import Any
 
 from aws_xray_sdk.core import patch_all, xray_recorder
 from mangum import Mangum
 
 from .config import config
+
+
+class ScheduledEventHandler:
+    # Dummy handler to gracefully handle keep warm events
+    @classmethod
+    def infer(cls, event, context, config) -> bool:
+        return "detail-type" in event and event["detail-type"] == "Scheduled Event"
+
+    def __init__(self, event, context, config) -> None:
+        self.event = event
+        self.context = context
+        self.config = config
+
+    @property
+    def body(self) -> bytes:
+        return {}
+
+    @property
+    def scope(self):
+        return {}
+
+    def __call__(self, response) -> dict[str, Any]:
+        return {
+            "status": 200,
+            "body": "Scheduled event processed"
+        }
+
 
 
 def create_lambda_handler(app):
@@ -16,14 +44,13 @@ def create_lambda_handler(app):
 
     @xray_recorder.capture("fastapi_request")
     def handler(event, context):
-        if event.get("some-key"):
-            # Do something or return, etc.
-            return
-
+        print("Received event: %s", event)
+        print("Received context: %s", context)
+        
         request_id: str = context.aws_request_id
-        event["X-Request-ID"] = request_id
+        event["headers"]["X-Request-ID"] = request_id
 
-        asgi_handler = Mangum(app, lifespan="off")
+        asgi_handler = Mangum(app, lifespan="off", custom_handlers=[ScheduledEventHandler])
         response = asgi_handler(
             event, context
         )  # Call the instance with the event arguments
