546d295b90c010da7dbe3e2d6d944e33fb94527d
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-05-09 10:25:51 +0100

    Removing project

--- templates/project/src/app/domain/example.py
+++
@@ -1,8 +0,0 @@
-from uuid import UUID
-from pydantic import BaseModel
-
-class ExampleDTO(BaseModel):
-    id: UUID
-    name: str
-    url: str
-    location: str
--- templates/project/src/app/interactor/api/lambda_wrapper.py
+++
@@ -1,7 +0,0 @@
-from hexrepo_api.lambda_wrapper import create_lambda_handler
-
-
-from .fastapi.main import app  # noqa
-
-
-handler = create_lambda_handler(app)
