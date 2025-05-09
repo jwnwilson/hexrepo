e4de83274685e24e461c9816e546e3cec3b3492b
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-05-09 10:47:06 +0100

    Removing test project after successful update

--- projects/test/hooks/pre_gen_project.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-"""
-{{ cookiecutter | stem_keys }}
-"""
-
-# Check the cleaned context.
-print("""{{ cookiecutter | jsonify }}""")
-
--- projects/test/src/tests/adaptor/sql/test_migrations.py
+++
@@ -1,9 +0,0 @@
-from pytest_alembic.tests import (
-    test_model_definitions_match_ddl as test_model_definitions_match_ddl,  # noqa
-)
-from pytest_alembic.tests import (
-    test_single_head_revision as test_single_head_revision,  # noqa
-)
-from pytest_alembic.tests import (
-    test_upgrade as test_upgrade,  # noqa
-)
