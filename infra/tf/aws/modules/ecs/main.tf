df663cd4ecf77a378e2afebdbf71182b5ffff295
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-05-24 18:55:16 +0100

    gateway nlb infra working

--- infra/tf/aws/modules/ecs/main.tf
+++ infra/tf/aws/modules/ecs/main.tf
@@ -489,10 +489,22 @@ resource "aws_api_gateway_rest_api" "main" {
 }
 
 # API Gateway Resource
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "api" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
   path_part   = "api"
+}
+
+resource "aws_api_gateway_resource" "api_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_resource.api.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{proxy+}"
 }
 
 # API Gateway Method
@@ -507,6 +519,29 @@ resource "aws_api_gateway_method" "proxy" {
   }
 }
 
+resource "aws_api_gateway_method" "api" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.api.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_method" "api_proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.api_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+
 resource "aws_api_gateway_method" "root" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   resource_id   = aws_api_gateway_rest_api.main.root_resource_id
@@ -517,13 +552,26 @@ resource "aws_api_gateway_method" "root" {
 # API Gateway Integration
 resource "aws_api_gateway_integration" "api" {
   rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.api.http_method
+
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  # Trailing slash is required for API Gateway to work
+  uri                     = "http://${aws_lb.main.dns_name}/api/"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.main.id
+}
+
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.proxy.http_method
 
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
   # Trailing slash is required for API Gateway to work
-  uri                     = "http://${aws_lb.main.dns_name}/api/{proxy}/"
+  uri                     = "http://${aws_lb.main.dns_name}/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.main.id
 
@@ -533,15 +581,15 @@ resource "aws_api_gateway_integration" "api" {
   }
 }
 
-resource "aws_api_gateway_integration" "main" {
+resource "aws_api_gateway_integration" "api_proxy" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_resource.api_proxy.id
+  http_method = aws_api_gateway_method.api_proxy.http_method
 
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
   # Trailing slash is required for API Gateway to work
-  uri                     = "http://${aws_lb.main.dns_name}/{proxy}"
+  uri                     = "http://${aws_lb.main.dns_name}/api/{proxy}/"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.main.id
 
