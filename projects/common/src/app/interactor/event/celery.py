37445eb27920617678e9bbe25a27aecaa3f08d78
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-06-04 08:44:18 +0100

    WIP setting up celery

--- projects/common/src/app/interactor/event/celery.py
+++ projects/common/src/app/interactor/event/celery.py
@@ -1,3 +1,4 @@
+from logging import config
 from celery import Celery
 
 from hexrepo_task.interactor.event.app import Dependency, TaskApp, TaskDTO
@@ -8,10 +9,28 @@ from app.domain.user import UserPermissionCreateDTO
 from ..dependencies import get_uow
 
 
-app = Celery('tasks', broker='sqs://')
+celery_app = Celery(
+    'tasks',
+    broker=config.CELERY_BROKER_URL.format(
+        aws_access_key_id=config.CELERY_AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=config.CELERY_AWS_SECRET_ACCESS_KEY
+    ),
+    backend=config.CELERY_RESULT_BACKEND
+)
+celery_app.conf.broker_transport_options = {
+    'region': config.REGION
+}
+
+if config.ENVIRONMENT == "local":
+    celery_app.conf.broker_transport_options["endpoint_url"] = "http://localhost:4566"
 
 
-@app.task
+@celery_app.task
+def test_task():
+    print("test task")
+
+
+@celery_app.task
 def create_example_task(task: TaskDTO, uow: SqlUOW = Dependency(get_uow)):
     user_dto: UserPermissionCreateDTO = UserPermissionCreateDTO(**task.params)
     uow.user.create(user_dto)
