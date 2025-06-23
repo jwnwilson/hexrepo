139e2cb19f17c33c686b51c5601597ffe2ab1938
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-06-23 08:07:22 +0100

    Setup initial map

--- projects/run_fe/app/(tabs)/index.tsx
+++ projects/run_fe/app/(tabs)/index.tsx
@@ -5,6 +5,32 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+import { OrbitControls } from '@react-three/drei/native';
+import Map from '@/components/map';
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 export default function HomeScreen() {
   return (
@@ -20,36 +46,16 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView style={[styles.stepContainer, { minHeight: 400 }]}>
+        {/* <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+          <OrbitControls />
+        </Canvas> */}
+        <Map />
       </ThemedView>
     </ParallaxScrollView>
   );
---
+++ projects/run_fe/components/map.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+import * as maptilersdk from '@maptiler/sdk';
+import "@maptiler/sdk/dist/maptiler-sdk.css";
+import './map.css';
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef<maptilersdk.Map | null>(null);
+  maptilersdk.config.apiKey = 'EQqTDQpQLmXSNBIrEdMM';
+  const tokyo = { lng: 139.753, lat: 35.6844 };
+  const zoom = 14;
+
+  useEffect(() => {
+    if (mapContainer.current) {
+      map.current = new maptilersdk.Map({
+        container: mapContainer.current,
+        style: maptilersdk.MapStyle.OUTDOOR,
+        center: [tokyo.lng, tokyo.lat],
+        zoom: zoom,
+        terrain: true,
+        terrainControl: true,
+        pitch: 30,
+        bearing: -100.86,
+        maxPitch: 50,
+      });
+    }
+  }, [tokyo, zoom]);
+
+  return (
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+}
