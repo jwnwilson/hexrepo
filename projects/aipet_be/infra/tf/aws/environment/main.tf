2e985116057401f312022346fa3858bc7c6bff06
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-08-28 18:01:52 +0100

    Simplifying template

--- projects/aipet_be/infra/tf/aws/environment/main.tf
+++ projects/aipet_be/infra/tf/aws/environment/main.tf
@@ -14,15 +14,14 @@ terraform {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-
-locals {
-  db_url = "postgresql+psycopg://postgres:{password}@${module.aipet_be_postgres.db_instance_endpoint}/${var.project}"
-  db_ro_url = module.aipet_be_postgres.db_instance_ro_endpoint != null ? "postgresql+psycopg://postgres:{password}@${module.aipet_be_postgres.db_instance_ro_endpoint}/${var.project}" : "postgresql+psycopg://postgres:{password}@${module.aipet_be_postgres.db_instance_endpoint}/${var.project}"
-}
-
 locals {
     account_id = data.aws_caller_identity.current.account_id
     region = data.aws_region.current.name
+    db_url            = "postgresql+psycopg://postgres:{password}@${module.common_postgres.db_instance_endpoint}/${var.project}"
+    db_ro_url         = module.common_postgres.db_instance_ro_endpoint != null ? "postgresql+psycopg://postgres:{password}@${module.common_postgres.db_instance_ro_endpoint}/${var.project}" : "postgresql+psycopg://postgres:{password}@${module.common_postgres.db_instance_endpoint}/${var.project}"
+    api_subdomain     = "common-${terraform.workspace}"
+    api_subdomain_ecs = "common-${terraform.workspace}-ecs"
+    app_url           = "https://${local.api_subdomain}.${var.domain}"
     common_env_vars = {
       ENVIRONMENT             = terraform.workspace
       PROJECT                 = var.project
@@ -32,12 +31,9 @@ locals {
       READ_REPLICA_ENABLED    = "false"
       DB_PASSWORD_SECRET_NAME = data.aws_secretsmanager_secret.db_secret.name
       TASK_QUEUE              = "${var.project}_${terraform.workspace}_tasks"
-      CLIENT_ID               = module.common_auth.client_id
-      USER_POOL_ID            = module.common_auth.user_pool_id
       ALLOWED_ORIGINS         = "*"
       LOG_JSON                = "true"
       ORIGIN_URL              = "https://${local.api_subdomain_ecs}.${var.domain}"
-      TASK_TABLE_NAME         = module.common_task_nosql.table_name
       LOG_LEVEL               = "INFO"
     }
 }
@@ -53,14 +49,49 @@ data "aws_vpc" "hexrepo" {
   }
 }
 
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.hexrepo.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["*private*"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.hexrepo.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+}
+
 data "aws_ecr_repository" "ecr_repo" {
   name                 = "hexrepo-${var.project}"
 }
 
+module "common_alb" {
+  source = "../../../../../../infra/tf/aws/modules/alb"
 
-
-
-
+  project            = var.project
+  environment        = terraform.workspace
+  aws_region         = var.aws_region
+  vpc_id             = data.aws_vpc.hexrepo.id
+  private_subnet_ids = data.aws_subnets.private.ids
+  public_subnet_ids  = data.aws_subnets.public.ids
+  vpc_cidr_blocks    = [data.aws_vpc.hexrepo.cidr_block]
+  container_port     = 8000
+  domain_name        = var.domain
+  subdomain_name     = local.api_subdomain_ecs
+  enabled            = false
+}
 
 module "aipet_be_ecs_api" {
   source             = "../../../../../../infra/tf/aws/modules/ecs"
@@ -70,7 +101,7 @@ module "aipet_be_ecs_api" {
   aws_region         = var.aws_region
   vpc_id             = data.aws_vpc.hexrepo.id
   private_subnet_ids = data.aws_subnets.private.ids
-  security_group_ids = [module.common_postgres.db_security_group_id]
+  security_group_ids = [module.postgres.db_security_group_id]
   target_group_arn   = module.common_alb.target_group_arn
   # This costs money
   container_insights = "disabled"
@@ -90,30 +121,7 @@ module "aipet_be_ecs_api" {
   task_memory   = 512
 }
 
-
-
-
-
-
-
-
-
-module "aipet_be_api_gateway" {
-  source = "../../../../../../infra/tf/aws/modules/apigateway"
-
-  environment       = terraform.workspace
-  lambda_invoke_arn = module.aipet_be_api.lambda_function_invoke_arn
-  lambda_name       = module.aipet_be_api.lambda_function_name
-  domain            = var.domain
-  api_subdomain     = "aipet_be-${terraform.workspace}"
-  project           = "aipet_be"
-  cognito_user_pool_arn = module.common_auth.user_pool_arn
-  # Auth handled in api middleware
-  auth_enabled          = false
-}
-
-
-module "aipet_be_postgres" {
+module "postgres" {
   source = "../../../../../../infra/tf/aws/modules/rds"
 
   environment       = terraform.workspace
@@ -126,9 +134,7 @@ data "aws_secretsmanager_secret" "db_secret" {
   arn = module.aipet_be_postgres.db_password_secret_arn
 }
 
-
-
-module "aipet_be_bucket" {
+module "main_bucket" {
   source = "../../../../../../infra/tf/aws/modules/s3"
 
   project     = "aipet_be"
