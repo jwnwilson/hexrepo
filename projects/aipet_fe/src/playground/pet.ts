a6477228e70bacec27cd7457eb1a18f9dc5e2c45
Noel Wilson <jwnwilson@hotmail.co.uk>
2025-08-30 23:02:00 +0100

    Fixing invalid prefix for bunny

--- projects/aipet_fe/src/playground/pet.ts
+++ projects/aipet_fe/src/playground/pet.ts
@@ -82,7 +82,7 @@ export class Pet {
     );
 
     // Create sprite manager and sprite
-    this.spriteManager = new SpriteManager("petSpriteManager", "/public/texture/bunny.png", 1, { width: 32, height: 32 }, this.scene);
+    this.spriteManager = new SpriteManager("petSpriteManager", "/texture/bunny.png", 1, { width: 32, height: 32 }, this.scene);
     this.sprite = new Sprite("petSprite", this.spriteManager);
     this.sprite.playAnimation(0, 4, true, 100);
     this.sprite.position = position;
